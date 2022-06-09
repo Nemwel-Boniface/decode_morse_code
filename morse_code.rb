86d6a29bb5be7437ee2c78decc6245c0392fff72
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-06-09 08:57:58 -0500

    Update method to display single morse code character in uppercase

--- morse_code.rb
+++ morse_code.rb
@@ -1,25 +1,20 @@
-# Create a method to decode a Morse code character, takes a string parameter, and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
-
-def decode_morse(string)
-  if string == ".-"
-    puts "a".upcase()
-   else
-     puts "Not morse code. \n Please input another message"
-  end
- end
-
-
-
- #  Create a method to decode an entire word in Morse code, takes a string parameter, and return the string representation. Every character in a word will be separated by a single space (e.g. decode_word("-- -.--") returns "MY").
-
-
 # Morse code Hash Dictionary
 
-morse_code_dict = {
+@morse_code_dict = {
   ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
   "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
   "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
   ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
   "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-  "--.." => "Z",
+  "--.." => "Z"
 }
+
+def morse_decoder(string)
+  @words = string.split('  ')
+  @words.each do |word|
+    process_code(word)
+  end
+end
+
+
+morse_decoder('-- -.--   -. .- -- .')
